4956d864316807c9a7987b241d8294d46e069c2b
David Gamero <david340804@gmail.com>
2020-09-17 12:08:49 -0400

    ignore invalid dates

--- scrape_covid_data.py
+++ scrape_covid_data.py
@@ -22,6 +22,9 @@ ROW_INDEX_CAMPUS_IMPACT = 3
 RE_POSITION_PAREN = re.compile('\(([0-9]+)\)')
 RE_INTEGERS = re.compile('([0-9]+)')
 
+RE_YEAR = re.compile(',[ ]+([0-9]{2,5})')
+MIN_ROW_YEAR = 2020
+
 
 def write_covid_data_csv(csv_path):
     data_reponse = requests.get(COVID_DATA_URL)
@@ -58,10 +61,10 @@ def write_covid_data_csv(csv_path):
     dates = []  # List of dates captured ordered descending
     date_2_num_cases = {}
 
-    extractions = [] # List of rows where a number of cases was extracted from the text
-    failed_rows = [] # List of rows that failed to be processed
+    extractions = []  # List of rows where a number of cases was extracted from the text
+    failed_rows = []  # List of rows that failed to be processed
 
-    def add_extraction(row_date,num_cases,extraction_type,source_text):
+    def add_extraction(row_date, num_cases, extraction_type, source_text):
         extractions.append({
             'date': row_date,
             'numCases': num_cases,
@@ -69,13 +72,47 @@ def write_covid_data_csv(csv_path):
             'sourceText': source_text
         })
 
+    def getYearInt(dateString):
+        re_year_result = RE_YEAR.search(dateString)
+        if re_year_result is not None and re_year_result.group(1) is not None:
+            return int(RE_YEAR.search(dateString).group(1))
+        return -1
+
+    def cleanText(text):
+        text = text.strip()
+
+        # Replace multiple spaces with a single space
+        text = re.sub(' +', ' ', text)
+
+        # Strip non-ascii characters
+        # https://stackoverflow.com/questions/8689795/how-can-i-remove-non-ascii-characters-but-leave-periods-and-spaces-using-python
+        text = re.sub(r'[^\x00-\x7f]', r'', text)
+
+        return text
+
     # Parse the number of covid cases from a given row
 
     def parse_num_cases(row):
         # Student singular
-        row_date = row[ROW_INDEX_DATE]
-        row_position = row[ROW_INDEX_POSITION]
-        row_campus_impact = row[ROW_INDEX_CAMPUS_IMPACT]
+        row_date = cleanText(row[ROW_INDEX_DATE])
+        row_position = cleanText(row[ROW_INDEX_POSITION])
+        row_campus_impact = cleanText(row[ROW_INDEX_CAMPUS_IMPACT])
+
+        # Verify valid date years
+        if(getYearInt(row_date) < MIN_ROW_YEAR):
+            print('INVALID DATE ROW IGNORED')
+            print('|'.join(row))
+            return -1
+
+        # Have we seen this date before? If not, initialize
+        if row_date not in date_2_num_cases:
+            date_2_num_cases[row_date] = 0
+            dates.append(row_date)
+
+        # Verify year doesn't go backwards in the rows
+        if(getYearInt(row_date) > getYearInt(dates[-1])):
+            row_date = dates[-1]  # Use last valid year
+            print('Replacing date for {}'.format(row))
 
         if(row_position == 'Student'):
             return 1
@@ -84,14 +121,14 @@ def write_covid_data_csv(csv_path):
         if(row_position in ['Staff', 'Staff member']):
             return 1
 
-        # POSITION '(N)' format 
+        # POSITION '(N)' format
         re_position_paren = RE_POSITION_PAREN.search(
             row_position)
         if(re_position_paren != None and re_position_paren.group(1) != None):
             # Extract the number between the parentheses
             num_cases = int(re_position_paren.group(1))
 
-            add_extraction(row_date,num_cases,'POSITION N',row_position)
+            add_extraction(row_date, num_cases, 'POSITION N', row_position)
             print('{} Extracted {} cases from Position="{}"'.format(
                 row_date, num_cases, row_position))
             return num_cases
@@ -103,7 +140,7 @@ def write_covid_data_csv(csv_path):
             num_cases = int(re_integers.group(1))
 
             # Alert warning for possibly lossy extraction
-            add_extraction(row_date,num_cases,'IMPACT',row_campus_impact)
+            add_extraction(row_date, num_cases, 'IMPACT', row_campus_impact)
             print('{} Extracted {} cases from text:  \n "{}"\n'.format(
                 row_date, num_cases, row_campus_impact))
 
@@ -116,19 +153,7 @@ def write_covid_data_csv(csv_path):
 
     # Parse case numbers and collapse on each date
     for row in text_rows:
-        date = row[ROW_INDEX_DATE]
-
-        # Replace multiple spaces with a single space
-        date = re.sub(' +', ' ', date)
-
-        # Strip non-ascii characters
-        # https://stackoverflow.com/questions/8689795/how-can-i-remove-non-ascii-characters-but-leave-periods-and-spaces-using-python
-        date = re.sub(r'[^\x00-\x7f]', r'', date)
-
-        # Have we seen this date before? If not, initialize
-        if date not in date_2_num_cases:
-            date_2_num_cases[date] = 0
-            dates.append(date)
+        date = cleanText(row[ROW_INDEX_DATE])  # TODO extract date cleanly
 
         # If the row was processed successfully
         if(parse_num_cases(row) != -1):
@@ -163,6 +188,7 @@ def write_covid_data_csv(csv_path):
     }
 
     return response
+
 
 if __name__ == "__main__":
     write_covid_data_csv(COVID_DATA_CSV)
